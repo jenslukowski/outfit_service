ee986e488bd8f00fe1ea689b2ae94fdfdbb0aade
jll <jens.lukowski@softwareschneiderei.de>
2018-10-08 21:01:32 +0200

    tests and rules for outfit recommendations

--- src/de/outfit/OutfitRecommendation.java
+++ src/de/outfit/OutfitRecommendation.java
@@ -1,19 +1,26 @@
 package de.outfit;
 
 public enum OutfitRecommendation {
-    veryLight(1),
-    light(2),
-    normal(3),
-    warm(4),
-    veryWarm(5);
+    veryLight(1, new Temperature(26)),
+    light(2, new Temperature(21)),
+    normal(3, new Temperature(15)),
+    warm(4, new Temperature(5)),
+    veryWarm(5, new Temperature(-273.15));
 
     private final int level;
+    private final Temperature minTemperature;
 
-    OutfitRecommendation(int level) {
+    OutfitRecommendation(int level, Temperature minTemperature) {
         this.level = level;
+        this.minTemperature = minTemperature;
     }
 
     public static OutfitRecommendation recommendOutfitFor(Temperature temperature) {
+        for (OutfitRecommendation recommendation : OutfitRecommendation.values()) {
+            if (recommendation.minTemperature.getTemperatureInCelsius() < temperature.getTemperatureInCelsius()) {
+                return recommendation;
+            }
+        }
         return veryLight;
     }
 
--- test/de/outfit/OutfitTest.java
+++ test/de/outfit/OutfitTest.java
@@ -2,7 +2,7 @@ package de.outfit;
 
 import org.junit.Test;
 
-import static de.outfit.OutfitRecommendation.veryLight;
+import static de.outfit.OutfitRecommendation.*;
 import static org.junit.Assert.assertEquals;
 
 public class OutfitTest {
@@ -10,5 +10,35 @@ public class OutfitTest {
     @Test
     public void temperatureGreaterThan26ShouldResultInVeryLight() {
         assertEquals(veryLight, OutfitRecommendation.recommendOutfitFor(new Temperature(27)));
+    }
+
+    @Test
+    public void temperatureOf26ShouldResultInLight() {
+        assertEquals(light, OutfitRecommendation.recommendOutfitFor(new Temperature(26)));
+    }
+
+    @Test
+    public void temperatureOf24ShouldResultInLight() {
+        assertEquals(light, OutfitRecommendation.recommendOutfitFor(new Temperature(24)));
+    }
+
+    @Test
+    public void temperatureOf21ShouldResultInNormal() {
+        assertEquals(normal, OutfitRecommendation.recommendOutfitFor(new Temperature(21)));
+    }
+
+    @Test
+    public void temperatureOf15ShouldResultInWarm() {
+        assertEquals(warm, OutfitRecommendation.recommendOutfitFor(new Temperature(15)));
+    }
+
+    @Test
+    public void temperatureOf5ShouldResultInVeryWarm() {
+        assertEquals(veryWarm, OutfitRecommendation.recommendOutfitFor(new Temperature(5)));
+    }
+
+    @Test
+    public void temperatureLessThan5ShouldResultInVeryWarm() {
+        assertEquals(veryWarm, OutfitRecommendation.recommendOutfitFor(new Temperature(-1)));
     }
 }
