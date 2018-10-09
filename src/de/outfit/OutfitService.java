2f424726f0a0e609d3785c5bc69c073712869719
jll <jens.lukowski@softwareschneiderei.de>
2018-10-09 09:44:08 +0200

    command line parameters for configuration of the service

--- src/de/outfit/OutfitService.java
+++ src/de/outfit/OutfitService.java
@@ -9,6 +9,7 @@ import io.undertow.util.Methods;
  * OutfitService
  * Provides a REST endpoint for an outfit recommendation based on the current temperature at a given location
  * The location can be given in the following ways:
+ *
  * /outfit?q=:city
  * /outfit?q=:city,:country
  * /outfit?id=:city_id
@@ -25,21 +26,35 @@ import io.undertow.util.Methods;
  *
  * Success Response:
  * Code: 200
- * Content:
+ * Content: {"temperature": 10.53, "temperature_unit": "Celsius", "outfit_level": 4}
  *
  * Error Response:
+ * Code 404
+ * Content: {"error": "Could not find location"}
+ *
  * Code 422
- * Content:
+ * Content: {"error": "No location given"}
+ *
+ * Code 503
+ * Content: {"error": "Could not make recommendation"}
  *
  * Example:
- * /outfit?q=Karlsruhe,DE
+ *
+ * Request: /outfit?q=Karlsruhe,DE
+ * Response: {"temperature": 10.53, "temperature_unit": "Celsius", "outfit_level": 4}
  */
 public class OutfitService {
 
     public static void main(String[] args) {
-        String host = "";
-        int port = 8080;
+        String host = valueFor(args, "--host=", "");
+        int port = intValueFor(args, "--port=", 8080);
         String apiKey = valueFor(args, "--key=", "");
+        if ("".equals(apiKey)) {
+            System.out.println("Please specify an api key with the --key= option");
+            System.out.println("Example:");
+            System.out.println("java -jar outfit_service.jar --key=12345");
+            return;
+        }
         final RoutingHandler routes = Handlers.routing()
                 .add(Methods.GET, "/outfit", new OutfitHandler(new OpenWeatherMap(apiKey)));
         final Undertow server = Undertow.builder()
@@ -57,5 +72,14 @@ public class OutfitService {
             }
         }
         return defaultValue;
+    }
+
+    private static int intValueFor(String[] args, String parameter, int defaultValue) {
+        try {
+            return Integer.parseInt(valueFor(args, parameter, String.valueOf(defaultValue)));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return defaultValue;
+        }
     }
 }
